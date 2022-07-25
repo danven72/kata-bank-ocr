49e0d68c0e70b19b919dcdcff934349a7e5a255a
Danilo Ventura <danilo.ventura@proactivity.it>
2022-07-25 15:17:51 +0200

    bugfix for file multilines

--- src/main/java/it/danven72/kata/ocr/FileTransformer.java
+++ src/main/java/it/danven72/kata/ocr/FileTransformer.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class FileTransformer {
-    private NumbersRow numberRows = new NumbersRow();
+    //private NumbersRow numberRows = new NumbersRow();
 
     public void transform(String pathAndFileName) throws Exception {
         URI res = ClassLoader.getSystemResource(pathAndFileName).toURI();
@@ -20,11 +20,20 @@ public class FileTransformer {
                 Stream<String> stream = Files.lines(Paths.get(res), StandardCharsets.UTF_8)
         ) {
             List<String> rowList = stream.toList();
+            int rowCounter =0;
+            NumbersRow numberRows = new NumbersRow();
             for (int i=0; i<rowList.size(); i++) {
-                numberRows.parseRow(i, rowList.get(i));
+                System.out.println("i:"+ i +" rowCounter:" + rowCounter + " -> "+rowList.get(i));
+                numberRows.parseRow(rowCounter, rowList.get(i));
+                ++rowCounter;
+                if (rowCounter==4) {
+                    System.out.println(numberRows.getOutputWithValidation(numberRows.getSimpleOutput()));
+                    numberRows.reset(); //= new NumbersRow();
+                    rowCounter = 0;
+                }
             }
             //System.out.println(numberRows.getSimpleOutput());
-            System.out.println(numberRows.getOutputWithValidation(numberRows.getSimpleOutput()));
+            //System.out.println(numberRows.getOutputWithValidation(numberRows.getSimpleOutput()));
         }
     }
 
--- src/main/java/it/danven72/kata/ocr/model/NumbersRow.java
+++ src/main/java/it/danven72/kata/ocr/model/NumbersRow.java
@@ -1,16 +1,22 @@
 package it.danven72.kata.ocr.model;
 
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class NumbersRow {
     private NumberElement[] numberElements = new NumberElement[9];
 
     public NumbersRow() {
-        for (int i=0; i< numberElements.length; i++) {
-            numberElements[i] = new NumberElement();
-        }
+        reset();
     }
 
     public void parseRow(int rowIndex, String row) {
-        if (rowIndex < 3) {
+        if (row.length() !=0) {
             if (row.length() != 27)
                 throw new IllegalArgumentException("Row length " + row.length() + " not valid: row length must be 27!");
             int elemNumber = 0;
@@ -70,6 +76,29 @@ public class NumbersRow {
         }
         return (wightedSum % 11) == 0;
     }
+
+    public List<String> transformFile(String pathAndFileName) throws Exception {
+        URI res = ClassLoader.getSystemResource(pathAndFileName).toURI();
+        List<String> result = new ArrayList<>();
+        try (
+                Stream<String> stream = Files.lines(Paths.get(res), StandardCharsets.UTF_8)
+        ) {
+            List<String> rowList = stream.toList();
+            for (int i=0; i<rowList.size(); i++) {
+                parseRow(i, rowList.get(i));
+            }
+            //System.out.println(numberRows.getSimpleOutput());
+            result.add((getOutputWithValidation(getSimpleOutput())));
+        }
+        return result;
+    }
+
+    public void reset() {
+        for (int i=0; i< numberElements.length; i++) {
+            numberElements[i] = new NumberElement();
+        }
+    }
+
 
     public static void main(String[] args) {
         NumbersRow nr = new NumbersRow();
